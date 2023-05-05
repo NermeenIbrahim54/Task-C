7ead0971413a8727ec5360f20f6a0bf464f5e485
Nermeen Ibrahim <inermeen54@gmail.com>
2023-05-05 13:05:55 +0200

    second

---
+++ c1.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    char x[100];
+    scanf("%[^\n]%*c", &x);
+  	printf("Hello, World!\n");
+    printf("%s",x);
+    return 0;}
